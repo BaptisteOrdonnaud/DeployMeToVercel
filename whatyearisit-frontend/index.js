8d85a05d4a225a2fc8d9f8984eab64e795f8fb36
Baptiste Ordonnaud <baptisteordonnaud@gmail.com>
2024-01-26 10:44:04 +0100

    dhgjlsbdfgjs

--- whatyearisit-frontend/index.js
+++ whatyearisit-frontend/index.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/year')
+fetch('https://deploy-me-to-vercel-backend-plum.vercel.app/year')
     .then(response => response.json())
     .then(data => {
         document.querySelector("#year").textContent = data.year
